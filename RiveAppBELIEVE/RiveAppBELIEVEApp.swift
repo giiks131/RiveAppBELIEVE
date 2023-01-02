09988b3fde04b2c2dc8743474e5f6ea5fc076b88
Alex Sh <giiks131@gmail.com>
2023-01-02 17:22:35 +0100

    Initial Commit

---
+++ RiveAppBELIEVE/RiveAppBELIEVEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAppBELIEVEApp.swift
+//  RiveAppBELIEVE
+//
+//  Created by Aleksandr Shapovalov on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RiveAppBELIEVEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
