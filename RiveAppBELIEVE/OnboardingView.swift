55b4af80bba86646f4499ddd62bcc03ba9ad7136
Alex Sh <giiks131@gmail.com>
2023-01-02 21:00:53 +0100

    update anim

--- RiveAppBELIEVE/OnboardingView.swift
+++ RiveAppBELIEVE/OnboardingView.swift
@@ -37,15 +37,18 @@ extension OnboardingView {
     }
     
     var buttonCourse: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 16) {
             Text("Learn design & code")
                 .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
                 .frame(width: 260, alignment: .leading)
             
             Text("Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.")
                 .customFont(.body)
-              
+                .opacity(0.7)
+                .frame(maxWidth: .infinity, alignment: .leading)
             
+            Spacer()
+              
             buttonStart.view()
                 .frame(width: 236, height: 64)
                 .overlay(
@@ -63,7 +66,13 @@ extension OnboardingView {
                 .onTapGesture {
                     buttonStart.play(animationName: "active")
             }
+            
+            Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.")
+                .customFont(.footnote)
+                .opacity(0.7)
         }
+        .padding(40)
+        .padding(.top, 40)
     }
     
 }
