b0afeec5a350ac766096041d6e9ad236efb260f5
Alex Sh <giiks131@gmail.com>
2023-01-03 21:01:19 +0100

    updated tabbar

--- RiveAppBELIEVE/TabBar.swift
+++ RiveAppBELIEVE/TabBar.swift
@@ -9,23 +9,14 @@ import SwiftUI
 import RiveRuntime
 
 struct TabBar: View {
+    @AppStorage("selectedTab") var selectedTab: Tab = .chat
     let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
     
     var body: some View {
         VStack {
             Spacer()
             HStack {
-                ForEach(tabItems) { item in
-                    Button {
-                        item.icon.setInput("active", value: true)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            item.icon.setInput("active", value: false)
-                        }
-                    } label: {
-                        item.icon.view()
-                            .frame(height: 36)
-                }
-                }
+               content
             }
             .padding(12)
             .background(Color("Background 2").opacity(0.8))
@@ -45,16 +36,60 @@ struct TabBar_Previews: PreviewProvider {
     }
 }
 
+extension TabBar {
+    var content: some View {
+        ForEach(tabItems) { item in
+            Button {
+                item.icon.setInput("active", value: true)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    item.icon.setInput("active", value: false)
+                }
+                withAnimation {
+                    selectedTab = item.tab
+                }
+            } label: {
+                item.icon.view()
+                    .frame(height: 36)
+                    .opacity(selectedTab == item.tab ? 1 : 0.5)
+                    .background(
+                        VStack {
+                            RoundedRectangle(cornerRadius: 2)
+                                .frame(width: selectedTab == item.tab ? 20 : 0, height: 4)
+                                .offset(y: -4)
+                                .opacity(selectedTab == item.tab ? 1 : 0)
+                            Spacer()
+                        }
+                    )
+        }
+        }
+    }
+    
+    
+}
 
 struct TabItem: Identifiable {
     var id = UUID()
     var icon: RiveViewModel
+    var tab: Tab
 }
 
 var tabItems = [
-    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
-    TabItem (icon: RiveViewModel (fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH" )),
-    TabItem(icon: RiveViewModel (fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER")),
-    TabItem (icon: RiveViewModel (fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL")),
-    TabItem (icon: RiveViewModel (fileName: "icons",  stateMachineName: "USER_Interactivity", artboardName: "USER"))
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT"), tab: .chat),
+    
+    TabItem (icon: RiveViewModel (fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH" ), tab: .search),
+    
+    TabItem(icon: RiveViewModel (fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER"), tab: .timer),
+    
+    TabItem (icon: RiveViewModel (fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL"), tab: .bell),
+    
+    TabItem (icon: RiveViewModel (fileName: "icons",  stateMachineName: "USER_Interactivity", artboardName: "USER"), tab: .user)
 ]
+
+
+enum Tab: String {
+    case chat
+    case search
+    case timer
+    case bell
+    case user
+}
